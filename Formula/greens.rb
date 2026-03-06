6da99d00262e0ae3cc054c23ad916a9320810446
yuvrajangadsingh <yuvrajangad.s@gmail.com>
2026-03-06 23:13:44 +0530

    greens v1.6.3

--- Formula/greens.rb
+++ Formula/greens.rb
@@ -1,8 +1,8 @@
 class Greens < Formula
   desc "Mirror private work contributions to your GitHub profile"
   homepage "https://github.com/yuvrajangadsingh/greens"
-  url "https://github.com/yuvrajangadsingh/greens/archive/refs/tags/v1.6.2.tar.gz"
-  sha256 "ef144f90dfb12a597b47d21322db86794559be20328ec3a80f2fe0ea15b53f35"
+  url "https://github.com/yuvrajangadsingh/greens/archive/refs/tags/v1.6.3.tar.gz"
+  sha256 "07c27142032c6ad394cc2e1389613e142be8a855fadfefb84c3f495387c248cd"
   license "MIT"
 
   depends_on "git"
